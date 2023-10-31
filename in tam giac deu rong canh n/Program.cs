0cb800ce0b5c25ec48e53322ccd9a58ecdbe755f
Quynh25052021 <147840643+Quynh25052021@users.noreply.github.com>
2023-10-31 13:52:09 +0700

    tamgiacdeu

---
+++ in tam giac deu rong canh n/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.Write("Nhập n: ");
+        int n = 0;
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            int space = n - i;
+
+            for (int j = 1; j <= space; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+            int space = n - i;
+
+            for (int j = 1; j <= space; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+
+}
