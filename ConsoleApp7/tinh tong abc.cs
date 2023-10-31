411a0ea7afbac8a1b96221112d3ecfa5b80b7667
Quynh25052021 <147840643+Quynh25052021@users.noreply.github.com>
2023-10-31 13:50:30 +0700

    baitoancong

---
+++ ConsoleApp7/tinh tong abc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());  
+        c = int.Parse(Console.ReadLine());
+
+        double sum = (a + b + c) / 3.00;
+        Console.WriteLine(Math.Round(sum, 2));
+    }       
+}
